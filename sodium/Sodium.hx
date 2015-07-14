b71a3b511a04df6ed071157919e7e90f7891c178
Nathan <nathan.huesken@posteo.de>
2015-07-14 17:35:59 +0200

    moved wrapper functions to its own class

--- sodium/Sodium.hx
+++ sodium/Sodium.hx
@@ -25,23 +25,4 @@ extern class Sodium {
   //helper functions
   static public function from_base64(str : String) : Uint8Array;
   static public function to_base64(a : Uint8Array) : String;
-
-  /** Convinience wrapper functions*/
-
-  // Memlimit/opslimit for passwordhash
-  static inline private var memlimit : Int = 32 * 1024 * 1024;
-  static inline private var opslimit : Int = 1024 * 1024;
-  static public function crypto_pwhash(password :String, salt : Uint8Array, keyLength : Int) : Uint8Array {
-    return crypto_pwhash_scryptsalsa208sha256(password, salt, keyLength);
-  }
-
-  // Encrypt/decrypt with secretkey WITHOUT nonce
-  static public function crypto_secretbox_easy_no_nonce(message : Uint8Array, key : Uint8Array, outputFormat : String = 'uint8array') : Uint8Array {
-    var nonce = new Uint8Array([for (i in 0...crypto_secretbox_NONCEBYTES) 0]);
-    return crypto_secretbox_easy(message, nonce, key, outputFormat);
-  }
-  static public function crypto_secretbox_open_easy_no_nonce(cipher : Uint8Array, key : Uint8Array, outputFormat : String = 'uint8array') : Uint8Array {
-    var nonce = new Uint8Array([for (i in 0...crypto_secretbox_NONCEBYTES) 0]);
-    return crypto_secretbox_open_easy(cipher, nonce, key, outputFormat);
-  }
 }
---
+++ sodium/SodiumWrapper.hx
@@ -0,0 +1,23 @@
+package sodium;
+
+import js.html.Uint8Array;
+class SodiumWrapper {
+  /** Convinience wrapper functions*/
+
+  // Memlimit/opslimit for passwordhash
+  static inline private var memlimit : Int = 32 * 1024 * 1024;
+  static inline private var opslimit : Int = 1024 * 1024;
+  static public function crypto_pwhash(password :String, salt : Uint8Array, keyLength : Int) : Uint8Array {
+    return Sodium.crypto_pwhash_scryptsalsa208sha256(password, salt, opslimit, memlimit, keyLength);
+  }
+
+  // Encrypt/decrypt with secretkey WITHOUT nonce
+  static public function crypto_secretbox_easy_no_nonce(message : Uint8Array, key : Uint8Array, outputFormat : String = 'uint8array') : Uint8Array {
+    var nonce = new Uint8Array([for (i in 0...Sodium.crypto_secretbox_NONCEBYTES) 0]);
+    return Sodium.crypto_secretbox_easy(message, nonce, key, outputFormat);
+  }
+  static public function crypto_secretbox_open_easy_no_nonce(cipher : Uint8Array, key : Uint8Array, outputFormat : String = 'uint8array') : Uint8Array {
+    var nonce = new Uint8Array([for (i in 0...Sodium.crypto_secretbox_NONCEBYTES) 0]);
+    return Sodium.crypto_secretbox_open_easy(cipher, nonce, key, outputFormat);
+  }
+}
